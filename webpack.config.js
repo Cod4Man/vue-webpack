340126f8c1ee0a61964a37f48797fdaaa9427418
Cod4Man <fresh_zz@163.com>
2020-02-12 22:28:28 +0800

    frist commit!

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path'); // 导入node.js的路径模块
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 根路径映射
+const htmpPlugin = new HtmlWebpackPlugin({ 
+    template: './src/index.html',
+    filename: 'index.html'
+});
+
+module.exports = {
+    mode: 'development', // or production
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    plugins:[htmpPlugin]
+}
